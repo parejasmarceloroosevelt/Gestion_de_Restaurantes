93cb62c7e7a549d71f30e1eab3b2e4538f5fc6d6
Roosevelt Parejas Marcelo <U23244078@utp.edu.pe>
2026-04-21 12:43:04 -0500

    Integracion de Spring boot

---
+++ src/main/java/com/Eugeo/gestion_restaurante/GestionRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.Eugeo.gestion_restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Eugeo/gestion_restaurante/GestionRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Eugeo.gestion_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

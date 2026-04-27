7c043e41a2b18570c592f81e3b13200b51a0f6a5
Loana Muñoz <u22314058@utp.edu.pe>
2026-04-26 19:02:20 -0500

    feat: creación de la clase Rol (Closes #4)

--- src/main/java/com/Eugeo/gestion_restaurante/service/RolService.java
+++ src/main/java/com/Eugeo/gestion_restaurante/service/RolService.java
@@ -21,4 +21,29 @@ public class RolService {
         return repository.findAll();
     }
 
+    public Rol findById(Long id){
+        return repository.findById(id)
+            .orElseThrow(()-> new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Rol no encontrado"));
+    }
+
+    public Rol create(Rol rol){
+        if (rol.getId() != null){
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "El rol ya tiene ID"
+            );
+        }
+        return repository.save(rol);
+    }
+
+    public Rol update(Long id, Rol rol){
+        Rol aux = findById(id);
+        aux.setNombre(rol.getNombre());
+        return repository.save(aux);
+    }
+
+    public void delete(Long id){
+        Rol rol = findById(id);
+        repository.deleteById(rol.getId());
+    }
 }
